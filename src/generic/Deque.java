940a833c5184a58d1cf0af23a25e7c330e62c91b
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2021-07-22 16:54:24 +0530

    Added generic DS

---
+++ src/generic/Deque.java
@@ -0,0 +1,99 @@
+package generic;
+
+public class Deque<T> {
+	class Node{
+		T data;
+		Node next;
+		Node previous;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	Node front;
+	Node rear;
+	
+	public void addRear(T item){
+		Node node = new Node(item);
+		if(front == null && rear == null){
+			front = node;
+			rear = node;
+		}
+		else{
+			rear.next = node;
+			node.previous = rear;
+			rear = node;
+		}
+	}
+	
+	public void addFront(T item){
+		Node node = new Node(item);
+		if(front == null && rear == null){
+			front = node;
+			rear = node;
+		}
+		else{
+			node.next = front;
+			front.previous = node;
+			front = node;
+		}
+	}
+	
+	public T removeRear(){
+		Node n = rear;
+		if(rear == front){
+			T data = rear.data;
+			rear = rear.next;
+			front = front.next;
+			return data;
+		}
+		
+		T data = n.data;
+		rear = n.previous;
+		n.previous.next = n.next;
+		return data;
+	}
+	
+	public T removeFront(){
+		Node n = front;
+		if(front == rear){
+			T data = front.data;
+			front = front.next;
+			rear = rear.next;
+			return data;
+		}
+		
+		T data = n.data;
+		front = n.next;
+		n.next.previous = n.previous;
+		return data;
+	}
+	
+	public int size(){
+		int i = 0;
+		Node n = front;
+		while(n != null){
+			i++;
+			n = n.next;
+		}
+		return i;
+	}
+	
+	public boolean isEmpty(){
+		if(front == null && rear == null)
+			return true;
+		return false;
+	}
+	
+	public String toString(){
+		String s = "[";
+		Node n = front;
+		while(n != null){
+			s = s+n.data;
+			if(n.next != null)
+				s = s+" "; 
+			n = n.next;
+		}
+		return s+"]";
+	}
+}
---
+++ src/generic/Stack.java
@@ -0,0 +1,71 @@
+package generic;
+
+public class Stack<T>{
+	
+	public class Node{
+		T data;
+		Node next;
+		public Node(T data){
+			this.data = data;
+		}
+	}
+	
+	Node top;
+
+	public void push(T item){
+		Node node = new Node(item);
+		if(top == null){
+			top = node;
+		}
+		else{
+			node.next = top;
+			top = node;
+		}
+	}
+	
+	public T pop(){
+		if(top == null){
+			throw new IndexOutOfBoundsException("Stack Underflow");
+		}
+		
+		T item = top.data;
+		top = top.next;
+		return item;
+	}
+	
+	public T peek(){
+		if(top == null){
+			throw new IndexOutOfBoundsException("Stack Empty");
+		}
+		T item = top.data;
+		return item;
+	}
+	
+	public boolean isEmpty(){
+		if(top == null)
+			return true;
+		return false;
+	}
+	
+	public int size(){
+		Node n = top;
+		int i = 0;
+		while(n!=null){
+			i++;
+			n = n.next;
+		}
+		return i;
+	}
+	
+	public String toString() {
+		String s = "[";
+		Node n = top;
+		while (n != null) {
+			s = s + n.data;
+			if (n.next != null)
+				s = s + " ";
+			n = n.next;
+		}
+		return s + "]";
+	}
+}
